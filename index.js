f0a1ad326fac519a6389e8923dac3edaf51c1dad
rémi  <remirobert33530@gmail.com>
2016-06-04 12:30:55 +0800

    up

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/webhook/', function (req, res) {
+  console.log("get events");
   let messaging_events = req.body.entry[0].messaging
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
